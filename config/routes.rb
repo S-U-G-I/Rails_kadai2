d9f19e131464e31a59ffd00e647c188789388a08
Mac-User <mac-user@makku-yuzanoMacBook-puro.local>
2020-01-26 03:49:55 +0900

    resources_on

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
+    resources :users
     root "users#index"
-    get "/users/new", to: "users#new"
-    post "/users",    to: "users#create"
-    get "/users/:id/edit", to: "users#edit"
-    patch "/users/:id", to: "users#update"
-    delete "/users/:id", to: "users#destroy"
 end
